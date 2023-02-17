d5a1d55e6e9f4bc40756f951de340f509508fe78
emma-atuks <emmanuel_atuluku@yahoo.co.uk>
2023-02-17 12:05:34 -0800

    Numbers base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints all the numbers in base 16 in lower cases
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+
+		putchar('\n');
+
+	return (0);
+}
