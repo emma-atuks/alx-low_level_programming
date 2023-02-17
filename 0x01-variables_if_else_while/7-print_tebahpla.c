435019d7230f7ea65106cfd555e6c60981bdaf8e
emma-atuks <emmanuel_atuluku@yahoo.co.uk>
2023-02-17 11:45:59 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints lower cases in reverse and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z';  le > = 'a'; le--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
