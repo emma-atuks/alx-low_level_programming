ca6cab6df57c82cfe094e2e10dfb8f32e1582656
emma-atuks <emmanuel_atuluku@yahoo.co.uk>
2023-02-16 20:56:23 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
-
-
 /**
- * main - prints exactly and that piece of the art is useful
- *
- * Return Always 0 (Success)
+ * main - Entry
+ * Return: 1
  */
-
 int main(void)
-
 {
-	write(2, "and that piece of the art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	write(1, "and that piece of the art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
