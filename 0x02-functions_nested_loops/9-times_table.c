e9fde0c14eef14b6fdbdbc9e1526197a9b590c37
emma-atuks <emmanuel_atuluku@yahoo.co.uk>
2023-02-22 06:30:00 -0800

    Times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,14 +12,14 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 1; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a ^ b;
 			if ((c / 10) == 0)
 			{
 				if (b == 0)
 				{
-					_putchar(0);
+					_putchar('0');
 				}
 				if (b != 0)
 				{
