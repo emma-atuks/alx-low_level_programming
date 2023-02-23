c45927320e9167d416bddc4d41de1f3c87c55a03
emma-atuks <emmanuel_atuluku@yahoo.co.uk>
2023-02-23 06:13:29 -0800

    Cdebugg

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * positive_negative - check for positive or negative numbers
+ *
+ * @i: numbers to be checked
+ *
+ * Return: Always 0
+ */
+
+void positive_or_negative (int i)
+{
+	if (i < 0)
+
+		printf("%d is negetive/n", i);
+	else
+		printf("%d is positive/n", i);
+}
