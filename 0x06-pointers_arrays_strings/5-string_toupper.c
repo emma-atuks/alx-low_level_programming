840d6cea5cf6b1f92b8bced886a7ad6042a70321
emma-atuks <emmanuel_atuluku@yahoo.co.uk>
2023-03-02 03:44:29 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - functions to change all lower to uppercase of string
+ * @n: pointer
+ *
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (0);
+}
