52e183e14bce512d6033878a60faa7f94102f4df
emma-atuks <emmanuel_atuluku@yahoo.co.uk>
2023-03-07 06:46:42 -0800

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,8 +14,8 @@ void print_chessboard(char (*a)[8])
 
 	for (j = 0; j < 8; j++)
 	{
-	for (k = 0; k < 8; k++)
-	_putchar(a[j][k]);
-	_putchar('\0');
+		for (k = 0; k < 8; k++)
+			_putchar(a[j][k]);
+		_putchar('\0');
 	}
 }
